cdb8ee1402f6e14dcf4e13f203c9de5de1258298
Benedikt Eiche <bene@rad-race.com>
2026-09-18 12:29:46 +0200

    Update site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://teamfinder.rad-race.com"),
+
   title: "RAD RACE Teamfinder",
   description: "Find your crew. Build your team. Ride together.",
 
